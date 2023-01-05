364f0442929ef5a83dbd4486d839fef3a8fc5c7c
zhangke200377 <zhangke200377@outlook.com>
2023-01-05 22:57:44 +0800

    delete temp file

--- unpackage/dist/dev/mp-weixin/pages/chatbox/chatbox.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
-}
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/\u5C11\u5E74\u5F3A\u56FD/wechat-end-master/pages/chatbox/chatbox.vue"]]);
-wx.createPage(MiniProgramPage);
