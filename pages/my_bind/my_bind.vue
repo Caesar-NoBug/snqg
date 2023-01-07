1e02c8d0408ead7484b5161914ac001b366a6b8a
Coder-Caesar <caesar666233@qq.com>
2023-01-07 23:16:14 +0800

    rectify

--- pages/my_bind/my_bind.vue
+++ pages/my_bind/my_bind.vue
@@ -21,6 +21,7 @@
 <script>
   import NavigateUtil from '../../utils/NavigateUtil';
   var userInfo = {
+    id: "123456",
     name: "张三",
     type: 0,
     avatar: "https://img12.360buyimg.com/imagetools/jfs/t1/143702/31/16654/116794/5fc6f541Edebf8a57/4138097748889987.png",
