8279251b6dab4f43b08930645e9258f355d173d6
Coder-Caesar <caesar666233@qq.com>
2023-02-04 11:23:12 +0800

    rectify

--- pages/chat/chat.vue
+++ pages/chat/chat.vue
@@ -1,8 +1,8 @@
 <template>
-  <!-- <chat_msg></chat_msg> -->
+  
   <top-bar-container title="主页">
   	<template #content>
-  		<chat_test></chat_test>
+  		
   	</template>
   </top-bar-container>
 </template>
