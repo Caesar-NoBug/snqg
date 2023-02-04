7d4f716dd74c3ec08b1110c90574ef61ac8162d3
gaolianbin <3506665131@qq.com>
2023-02-04 11:48:32 +0800

    进行对关注的编写

--- pages/favorite/favorite.vue
+++ pages/favorite/favorite.vue
@@ -1,22 +1,8 @@
 <template>
-  <view>
-    
-  </view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        
-      }
-    },
-    methods: {
-      
-    }
-  }
 </script>
 
 <style>
-
 </style>
